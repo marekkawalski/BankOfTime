4fceee1f7a313543ed3c68c41bd313d3fee211b0
marekkawalski <marek.kawalski@gmail.com>
2022-08-23 20:17:13 +0200

    Add spring app -backend

---
+++ backend/BankOfTime/src/main/java/com/bankoftime/BankOfTimeApplication.java
@@ -0,0 +1,13 @@
+package com.bankoftime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankOfTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankOfTimeApplication.class, args);
+    }
+
+}
