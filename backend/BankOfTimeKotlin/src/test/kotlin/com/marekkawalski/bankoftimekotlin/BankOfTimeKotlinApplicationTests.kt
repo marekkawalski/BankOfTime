51ea79f75dd66ba6a49d9a70cf4bcebaf2d9b47d
marekkawalski <marek.kawalski@gmail.com>
2022-08-27 22:23:45 +0200

    Change java to kotlin. Dockerize backend.

---
+++ backend/BankOfTimeKotlin/src/test/kotlin/com/marekkawalski/bankoftimekotlin/BankOfTimeKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.marekkawalski.bankoftimekotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BankOfTimeKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
