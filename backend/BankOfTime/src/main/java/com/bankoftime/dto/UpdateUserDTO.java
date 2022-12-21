942ca8ad0274a5c0d3a03fb6a7a27afc3b1e4ae0
marekkawalski <marek.kawalski@gmail.com>
2022-12-21 17:47:55 +0100

    Add updating user profile data

---
+++ backend/BankOfTime/src/main/java/com/bankoftime/dto/UpdateUserDTO.java
@@ -0,0 +1,26 @@
+package com.bankoftime.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public record RegistrationDTO(
+        Long id,
+        @Size(min = 3, max = 25, message = "Author first name must be between 3 to 25 letters")
+        @NotBlank(message = "First name is mandatory")
+        String firstName,
+        @Size(min = 3, max = 25, message = "Author last name must be between 3 to 25 letters")
+        @NotBlank(message = "LastName is mandatory")
+        String lastName,
+
+        @NotBlank(message = "Password is mandatory")
+        String password,
+        @Email
+        @NotBlank(message = "Email is mandatory")
+        String email,
+        String city,
+        String country,
+        String phoneNumber,
+        String aboutMe
+) {
+}
